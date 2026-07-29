eb7738a40e554a91a3f65d5be212fea2d82629a9
FireFlows <62861057+FireFlows@users.noreply.github.com>
2026-07-29 02:20:20 -0400

    fire

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,11 +1,16 @@
 const theme = localStorage.getItem('data-theme');
 document.documentElement.setAttribute("data-theme",theme);
 
-function setDarkTheme() {
-  localStorage.setItem("data-theme","dark")
-  document.documentElement.setAttribute("data-theme","dark");
-	}
-function setLightTheme() {
+function setTheme() {
+  var x = document.getElementById("theme");
+  if (x.innerHTML === "Light") {
+    x.innerHTML = "Dark";
     localStorage.setItem("data-theme","light")
     document.documentElement.setAttribute("data-theme","light");
-	}
+  } 
+  else {
+    x.innerHTML = "Light";
+    localStorage.setItem("data-theme","dark")
+  document.documentElement.setAttribute("data-theme","dark");
+  }
+}
