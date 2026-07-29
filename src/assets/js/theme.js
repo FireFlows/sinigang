c0b14e358272a0c6782412500bd0b3865cfbb513
FireFlows <62861057+FireFlows@users.noreply.github.com>
2026-07-29 02:38:26 -0400

    e

--- src/assets/js/theme.js
+++ src/assets/js/theme.js
@@ -1,13 +1,11 @@
-const theme = localStorage.getItem('data-theme');
-document.documentElement.setAttribute("data-theme",theme);
-
+document.documentElement.setAttribute("data-theme",localStorage.getItem('data-theme'));
 function setTheme() {
   var x = document.getElementById("theme");
   if (x.innerHTML === "Light") {
     x.innerHTML = "Dark";
-    localStorage.setItem("data-theme","light")
     document.documentElement.setAttribute("data-theme","light");
-  } 
+    localStorage.setItem("data-theme","light")  
+} 
   else {
     x.innerHTML = "Light";
     localStorage.setItem("data-theme","dark")
